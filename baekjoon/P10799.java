259e28d048f9ddf0fefe11d09acd6570a9f816d5
ChanJin Yun <84430221+ckswls147@users.noreply.github.com>
2023-01-16 23:38:55 +0900

    쇠막대기

---
+++ baekjoon/P10799.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = bf.readLine();
+        Stack<Character> stack = new Stack<>();
+
+        int result = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '(') {
+                stack.push('(');
+                continue;
+            }
+            if (input.charAt(i) == ')') {
+                stack.pop();
+
+                if (input.charAt(i - 1) == '(') { 
+                    result += stack.size(); 
+                } else { 
+                    result++; 
+                }
+            }
+        }
+
+        bw.write(result + "\n");
+        bw.flush();
+        bf.close();
+        bw.close();
+    }
+
+}
